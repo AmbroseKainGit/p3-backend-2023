2bc09d33411d4ce67f81f91d2969be681f347f02
Cristian Ceballos <cmceballos88@gmail.com>
2023-05-28 00:34:01 +0200

    Add enpoint to get the catory by id

--- src/Controllers/category.controller.ts
+++ src/Controllers/category.controller.ts
@@ -4,3 +4,12 @@ export const getAllCategories = async (req: Request, res: Response) => {
     const categories = await prisma.category.findMany();
     res.status(200).json({ data: categories, ok: true });
 };
+
+export const getCategoryById = async (req: Request, res: Response) => {
+    const categoryId = parseInt(req.params.id);
+    const category = await prisma.category.findUnique({ where: { id: categoryId } });
+    if (!category) {
+        return res.status(404).json({ error: 'Category not found' });
+    }
+    res.json({ data: category, ok: true });
+};
--- src/Routes/category.ts
+++ src/Routes/category.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { errorChecked } from '../Utils/handler';
-import { getAllCategories } from '../Controllers/category.controller';
+import {
+    getAllCategories,
+    getCategoryById
+} from '../Controllers/category.controller';
 
 const categoryRouter = Router();
 
 categoryRouter.get('/', errorChecked(getAllCategories));
+categoryRouter.get('/:id', errorChecked(getCategoryById));
 export { categoryRouter };
