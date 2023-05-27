a70d5ac98e76199ec066d1daf280d9ad3d60e4d2
Cristian Ceballos <cmceballos88@gmail.com>
2023-05-27 22:37:17 +0200

    Add error handler and complete the initial schema of models for the restaurant db

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import { defaultErrorHandler } from "./handler";
 dotenv.config();
 const app = express();
 const { SERVER_PORT } = process.env;
+
+app.use(defaultErrorHandler);
 app.listen(SERVER_PORT, () => {
   console.log(`Restaurant API listening on: ${SERVER_PORT}`);
 });
